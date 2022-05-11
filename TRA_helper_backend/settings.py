129bb2f3b2bcb7c3101dc11c80852be6f306e7b7
Penguin.jpg <sky026912@gmail.com>
2022-05-11 13:50:32 +0800

    使用django-rest-auth套件做登入

--- TRA_helper_backend/settings.py
+++ TRA_helper_backend/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework.authtoken",
     "rest_framework",
+    "rest_auth",
     "accounts",
     "tickets",
 ]
@@ -75,7 +77,7 @@ TEMPLATES = [
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.BasicAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
     ]
 }
 
